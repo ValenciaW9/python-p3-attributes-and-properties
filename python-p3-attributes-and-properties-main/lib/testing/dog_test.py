e0a30777e8462ffc7b434ab3d8f6ebb9825d193e
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-27 19:58:03 -0500

    python-p3-attributes-and-properties/
    python-p3-attributes-and-properties/

--- python-p3-attributes-and-properties-main/lib/testing/dog_test.py
+++ python-p3-attributes-and-properties-main/lib/testing/dog_test.py
@@ -1,125 +1,56 @@
 #!/usr/bin/env python3
+
 from dog import Dog
-from person import Person
+
 import io
 import sys
-
 
 class TestDog:
     '''Dog in dog.py'''
 
     def test_is_class(self):
         '''is a class with the name "Dog".'''
-        fido = Dog(name="Fido")
-        assert (isinstance(fido, Dog))
-
+        fido = Dog()
+        assert(type(fido) == Dog)
+        
     def test_name_not_empty(self):
         '''prints "Name must be string between 1 and 25 characters." if empty string.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        try:
-            Dog(name="")
-        except ValueError as e:
-            assert str(
-                e) == "Name must be a string between 1 and 25 characters."
+        Dog(name="")
         sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
 
     def test_name_string(self):
         '''prints "Name must be string between 1 and 25 characters." if not string.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        try:
-            Dog(name=123)
-        except ValueError as e:
-            assert str(
-                e) == "Name must be a string between 1 and 25 characters."
+        Dog(name=123)
         sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
 
     def test_name_under_25(self):
         '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        try:
-            Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
-        except ValueError as e:
-            assert str(
-                e) == "Name must be a string between 1 and 25 characters."
+        Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
         sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
 
     def test_valid_name(self):
         '''saves name if string between 1 and 25 characters.'''
         fido = Dog(name="Fido")
-        assert (fido.name == "Fido")
+        assert(fido.name == "Fido")
 
     def test_breed_not_in_list(self):
         '''prints "Breed must be in list of approved breeds." if not in breed list.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        try:
-            Dog(breed="Human")
-        except ValueError as e:
-            assert str(e) == "Breed must be in the list of approved breeds."
+        Dog(breed="Human")
         sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Breed must be in list of approved breeds.\n")
 
     def test_breed_in_list(self):
         '''saves breed if in breed list.'''
         fido = Dog(breed="Pug")
-        assert (fido.breed == "Pug")
-
-
-class TestPerson:
-    '''Person in person.py'''
-
-    def test_name_not_empty(self):
-        '''prints "Name must be string between 1 and 25 characters." if empty string.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        try:
-            Person(name="")
-        except ValueError as e:
-            assert str(
-                e) == "Name must be a string between 1 and 25 characters."
-        sys.stdout = sys.__stdout__
-
-    def test_name_string(self):
-        '''prints "Name must be string between 1 and 25 characters." if not string.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        try:
-            Person(name=123)
-        except ValueError as e:
-            assert str(
-                e) == "Name must be a string between 1 and 25 characters."
-        sys.stdout = sys.__stdout__
-
-    def test_name_under_25(self):
-        '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        try:
-            Person(
-                name="What do people do on their day off? Can't lie around - that's their job.")
-        except ValueError as e:
-            assert str(
-                e) == "Name must be a string between 1 and 25 characters."
-        sys.stdout = sys.__stdout__
-
-    def test_valid_name(self):
-        '''saves name if string between 1 and 25 characters.'''
-        person = Person(name="Ada")
-        assert (person.name == "Ada")
-
-    def test_job_not_in_list(self):
-        '''prints "Job must be in list of approved jobs." if not in job list.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        try:
-            Person(job="Teacher")
-        except ValueError as e:
-            assert str(e) == "Job must be in the list of approved jobs."
-        sys.stdout = sys.__stdout__
-
-    def test_job_in_list(self):
-        '''saves job if in job list.'''
-        person = Person(job="Sales")
-        assert (person.job == "Sales")
+        assert(fido.breed == "Pug")
